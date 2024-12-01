1fabdf58aa94a800e0ec2eff2e0c1ae59a0218b5
Emmanuel Rotich <71790505+mikemanuu@users.noreply.github.com>
2024-12-01 19:38:45 +0300

    commit

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -15,6 +15,7 @@ CREATE INDEX idx_property_id ON Property(PropertyID);
 
 --Measure query perfotmance before indexing using EXPLAIN $ ANALYZE
 EXPLAIN SELECT * FROM User INNER JOIN Booking ON User.UserID = Booking.UserID;
-
+ANALYZE SELECT * FROM User INNER JOIN Booking ON User.UserID = Booking.UserID;
 --Measure query perfotmance after indexing using EXPLAIN $ ANALYZE
 EXPLAIN SELECT * FROM User INNER JOIN Booking ON User.UserID = Booking.UserID;
+ANALYZE SELECT * FROM User INNER JOIN Booking ON User.UserID = Booking.UserID;
